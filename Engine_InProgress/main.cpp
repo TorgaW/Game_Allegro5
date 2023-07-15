ca6875839f11f618c9fccd2aff70ea2715a1d44b
lime <teo.carn@gmail.com>
2023-07-15 14:42:52 +0300

    t

--- Engine_InProgress/main.cpp
+++ Engine_InProgress/main.cpp
@@ -3,30 +3,29 @@
 
 int main(int argc, char const *argv[])
 {
-    std::vector<ObjRef<Object>> refs;
-    for (size_t i = 0; i < 50000; i++)
-    {
-        refs.push_back(ObjectManager::CreateObject<Object>("Aboba", "class_Object"));
-    }
-    for (size_t i = 0; i < refs.size(); i++)
-    {
-        ObjectManager::DestroyObject(refs[i]);
-    }
-    refs.clear();
-    for (size_t i = 0; i < 50000; i++)
-    {
-        refs.push_back(ObjectManager::CreateObject<Object>("Aboba", "class_Object"));
-    }
+    // std::vector<ObjRef<Object>> refs;
+    // for (size_t i = 0; i < 50000; i++)
+    // {
+    //     refs.push_back(ObjectManager::CreateObject<Object>("Aboba", "class_Object"));
+    // }
+    // for (size_t i = 0; i < refs.size(); i++)
+    // {
+    //     ObjectManager::DestroyObject(refs[i]);
+    // }
+    // refs.clear();
+    // for (size_t i = 0; i < 50000; i++)
+    // {
+    //     refs.push_back(ObjectManager::CreateObject<Object>("Aboba", "class_Object"));
+    // }
 
+    auto t = ObjectManager::CreateObject<Object>("Aboba", "class_Object");
 
     auto s1 = std::chrono::high_resolution_clock::now();
-    for (size_t i = 0; i < refs.size(); i++)
-    {
-        refs[i]->Begin();
-    }
+
+    ObjectManager::DestroyObject(t);
 
     auto s2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(s2 - s1);
-    std::cout << duration.count() << " microseconds";
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(s2 - s1);
+    std::cout << duration.count() << " nanoseconds";
     return 0;
 }
