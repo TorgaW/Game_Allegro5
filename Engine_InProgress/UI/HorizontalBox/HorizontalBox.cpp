0689701e798475406aec342530fe42e43d2d0051
Kartashov Dmitriy <kartashov104@gmail.com>
2023-08-02 13:02:26 +0300

    123

--- Engine_InProgress/UI/HorizontalBox/HorizontalBox.cpp
+++ Engine_InProgress/UI/HorizontalBox/HorizontalBox.cpp
@@ -69,23 +69,58 @@ void HorizontalBox::Update(float delta)
         case Alignment::UpperLeft:
         {
             const float containerWidth = widget_transform.size.x;
+            const float containerHeight = widget_transform.size.y;
 
             if (widget_children.size() == 1)
             {
-                widget_children[0]->widget_transform.position.x = widget_transform.position.x - containerWidth / 2.0f + widget_children[0]->widget_transform.size.x / 2.0f;
-                widget_children[0]->widget_transform.position.y = widget_transform.position.y - widget_transform.size.y / 2.0f + widget_children[0]->widget_transform.size.y / 2.0f;
+                if (horizontal_stretch)
+                {
+                    widget_children[0]->widget_transform.size.x += containerWidth - widget_children[0]->widget_transform.size.x;
+                    widget_children[0]->widget_transform.position.x = widget_transform.position.x;
+                }
+                else
+                    widget_children[0]->widget_transform.position.x = widget_transform.position.x - containerWidth / 2.0f + widget_children[0]->widget_transform.size.x / 2.0f;
+
+                if (vertical_stretch)
+                {
+                    widget_children[0]->widget_transform.size.y += containerHeight - widget_children[0]->widget_transform.size.y;
+                    widget_children[0]->widget_transform.position.y = widget_transform.position.y - widget_transform.size.y / 2.0f + widget_children[0]->widget_transform.size.y / 2.0f;
+                }
+                else
+                    widget_children[0]->widget_transform.position.y = widget_transform.position.y - widget_transform.size.y / 2.0f + widget_children[0]->widget_transform.size.y / 2.0f;
             }
             else if (widget_children.size() > 1)
             {
+                // Count total elements width with gaps to set them in the center
+                float totalElementsWidth{0.f};
+                for (size_t i = 0; i < widget_children.size(); i++)
+                {
+                    totalElementsWidth += widget_children[i]->widget_transform.size.x;
+                }
+                totalElementsWidth += (widget_children.size() - 1) * elements_gap_x;
+
+                // Counting additional size in order to stretch all the elements
+                float widthToAddToEachElement = 0.f;
+                float heightToAddToEachElement = 0.f;
+                if (horizontal_stretch)
+                    widthToAddToEachElement = (containerWidth - totalElementsWidth) / widget_children.size();
+                
                 // Initial values
                 float currentX = widget_transform.position.x - containerWidth / 2.0f;
                 float currentY = widget_transform.position.y - widget_transform.size.y / 2.0f;
-        
+
                 for (size_t i = 0; i < widget_children.size(); i++)
                 {
+                    // Stretch the elements on the horizontal axis
+                    widget_children[i]->widget_transform.size.x += widthToAddToEachElement;
+
+                    // Stretch the elements on the vertical axis
+                    if(vertical_stretch) widget_children[i]->widget_transform.size.y += containerHeight - widget_children[i]->widget_transform.size.y;
+
                     // Position the widget in the current row.
                     widget_children[i]->widget_transform.position.x = currentX + widget_children[i]->widget_transform.size.x / 2.0f;
                     widget_children[i]->widget_transform.position.y = currentY + widget_children[i]->widget_transform.size.y / 2.0f;
+
                     // Move the currentX position to the right for the next widget in the row.
                     currentX += elements_gap_x + widget_children[i]->widget_transform.size.x;
                 }
